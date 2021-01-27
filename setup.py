ba12542be766978a45dd2ee4babd3f49e3aef311
ssrug <63511216+ssrug@users.noreply.github.com>
2021-01-27 17:13:46 +0900

    Use repo as package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='Python-Charting-Indicators',
+      version='0.0.1',
+      description='python package to access the binance api',
+      author='EasyAI, ssrug',
+      url='https://github.com/ssrug/binance_api',
+      packages=['technical_indicators'],
+      install_requires=[
+          'numpy'
+        ]
+      )
